5ef61b084086117ad5249f779e8dc3e2ee2c0134
Tyler Lott <Tylerlott0126@gmail.com>
2019-07-23 15:24:34 -0600

    update2_working_logic

--- Statics.py
+++ Statics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 import math
 
 SUPPORTS = 1
@@ -107,71 +108,68 @@ class Fixed_Connection:
 
 def sum_forces_x(list_of_Forces):
     sum_x = 0
-    for i in list_of_Forces:
-        if list_of_Forces[i].get_magnitude == 'find':
+    for i in range(len(list_of_Forces)):
+        if list_of_Forces[i].get_magnitude() == 'find':
             continue
         else:
-            sum_x += list_of_Forces[i].get_magnitude * list_of_Forces[i].find_x_comp
-    return sum_x
+            sum_x += list_of_Forces[i].get_magnitude() * list_of_Forces[i].find_x_comp()
+    return -sum_x
 
 
 def sum_forces_y(list_of_Forces):
     sum_y = 0
-    for i in list_of_Forces:
-        if list_of_Forces[i].get_magnitude == 'find':
+    for i in range(len(list_of_Forces)):
+        if list_of_Forces[i].get_magnitude() == 'find':
             continue
         else:
-            sum_y += list_of_Forces[i].get_magnitude * list_of_Forces[i].find_y_comp
-    return sum_y
+            sum_y += list_of_Forces[i].get_magnitude() * list_of_Forces[i].find_y_comp()
+    return -sum_y
 
 
-def sum_moments_at_point(x_point, y_point, list_of_Moments, list_of_Forces):
+def sum_moments_at_point(x_point, y_point, list_of_Forces, list_of_Moments):
     sum_m = 0
-    for i in list_of_Forces:
-        if list_of_Forces[i].magnitude == 'find':
+    for i in range(len(list_of_Forces)):
+        if list_of_Forces[i].get_magnitude() == 'find':
             continue
         else:
-            if list_of_Forces[i].find_x_comp and list_of_Forces[i].find_y_comp != 0:
-                sum_m += ((list_of_Forces.find_x_comp * list_of_Forces.get_magnitude)
-                          * (list_of_Forces[i].get_x_location - x_point))
-            elif list_of_Forces[i].find_x_comp == 0:
-                sum_m += ((list_of_Forces.find_y_comp * list_of_Forces.get_magnitude)
-                           * (list_of_Forces[i].get_y_location - y_point))
-            elif list_of_Forces[i].find_y_comp == 0:
-                sum_m += ((list_of_Forces.find_x_comp * list_of_Forces.get_magnitude)
-                          * (list_of_Forces[i].get_x_location - x_point))
-    for i in list_of_Moments:
-        if list_of_Moments[i].get_magnitude == 'find':
+            if list_of_Forces[i].find_x_comp() >= .001 and list_of_Forces[i].find_y_comp() >= .001:
+                sum_m += ((list_of_Forces[i].find_x_comp() * list_of_Forces[i].get_magnitude())
+                          * (list_of_Forces[i].get_x_location() - x_point))
+            elif list_of_Forces[i].find_x_comp() <= .001:
+                sum_m += ((list_of_Forces[i].find_y_comp() * list_of_Forces[i].get_magnitude())
+                          * (list_of_Forces[i].get_x_location() - x_point))
+            elif list_of_Forces[i].find_y_comp() <= .001:
+                sum_m += ((list_of_Forces[i].find_x_comp() * list_of_Forces[i].get_magnitude())
+                          * (list_of_Forces[i].get_y_location() - y_point))
+    for i in range(len(list_of_Moments)):
+        if list_of_Moments[i].get_magnitude() == 'find':
             continue
         else:
-            sum_m += list_of_Moments[i].find_mag_with_dir
+            sum_m += list_of_Moments[i].find_mag_with_dir()
 
-    return sum_m
+    return -sum_m
 
 
 def forces_x(list_of_Forces):
     list_of_forces_x = []
-    for i in list_of_Forces:
-        if list_of_Forces[i].get_magnitude != 'find':
+    for i in range(len(list_of_Forces)):
+        if list_of_Forces[i].get_magnitude() != 'find':
             list_of_forces_x.append(0)
-            continue
         else:
-            value = list_of_Forces[i].find_x_comp
+            value = list_of_Forces[i].find_x_comp()
             list_of_forces_x.append(value)
-        list_of_forces_x.append(0)
 
     return list_of_forces_x
 
 
 def forces_y(list_of_Forces):
     list_of_forces_y = []
-    for i in list_of_Forces:
-        list_of_forces_y.append(0)
-        if list_of_Forces[i].get_magnitude != 'find':
+    for i in range(len(list_of_Forces)):
+        if list_of_Forces[i].get_magnitude() != 'find':
             list_of_forces_y.append(0)
             continue
         else:
-            value = list_of_Forces[i].find_y_comp
+            value = list_of_Forces[i].find_y_comp()
             list_of_forces_y.append(value)
 
     return list_of_forces_y
@@ -179,33 +177,28 @@ def forces_y(list_of_Forces):
 
 def moment_eq_at_point(x_location, y_location, list_of_Forces, list_of_Moments):
     list_of_moments = []
-    for i in list_of_Forces:
-        if list_of_Forces[i].get_x_location == x_location and list_of_Forces[i].get_y_location == y_location:
-            list_of_moments.append(0)
-            list_of_moments.append(0)
-            continue
-        elif list_of_Forces[i].get_magnitude != 'find':
-            list_of_moments.append(0)
+    for i in range(len(list_of_Forces)):
+        if list_of_Forces[i].get_x_location() == x_location and list_of_Forces[i].get_y_location() == y_location:
             list_of_moments.append(0)
             continue
-        elif list_of_Forces[i].get_magnitude == 'find':
-            if list_of_Forces[i].find_x_comp and list_of_Forces[i].find_y_comp != 0:
-                value = list_of_Forces[i].find_x_comp * list_of_Forces[i].get_x_location
-                list_of_moments.append(value)
-                list_of_moments.append(0)
-                continue
-            elif list_of_Forces[i].find_x_comp == 0:
-                value = list_of_Forces[i].find_y_comp * list_of_Forces[i].get_y_location
-                list_of_moments.append(0)
+        elif list_of_Forces[i].get_magnitude() != 'find':
+            list_of_moments.append(0)
+            continue
+        elif list_of_Forces[i].get_magnitude() == 'find':
+            if list_of_Forces[i].find_x_comp() <= .001 and list_of_Forces[i].find_y_comp() <= 0.001:
+                value = list_of_Forces[i].find_x_comp() * (list_of_Forces[i].get_y_location() - y_location)
                 list_of_moments.append(value)
                 continue
-            elif list_of_Forces[i].find_y_comp == 0:
-                value = list_of_Forces[i].find_x_comp * list_of_Forces[i].get_x_location
+            elif list_of_Forces[i].find_x_comp() <= 0.001:
+                value = list_of_Forces[i].find_y_comp() * (list_of_Forces[i].get_x_location() - x_location)
                 list_of_moments.append(value)
-                list_of_moments.append(0)
                 continue
-    for i in list_of_Moments:
-        if list_of_Moments[i].get_magnitude != 'find':
+            elif list_of_Forces[i].find_y_comp() <= 0.001:
+                value = list_of_Forces[i].find_x_comp() * (list_of_Forces[i].get_y_location() - y_location)
+                list_of_moments.append(value)
+                continue
+    for i in range(len(list_of_Moments)):
+        if list_of_Moments[i].get_magnitude() != 'find':
             list_of_moments.append(0)
             continue
         elif list_of_Moments[i]:
@@ -214,29 +207,108 @@ def moment_eq_at_point(x_location, y_location, list_of_Forces, list_of_Moments):
     return list_of_moments
 
 
-def find_equations(list_of_Forces, list_of_Moments):
-    a_matrix = np.array([forces_x(list_of_Forces)], [forces_y(list_of_Forces)])
+def find_a_matrix(list_of_Forces, list_of_Moments):
+    a_matrix = [forces_x(list_of_Forces), forces_y(list_of_Forces)]
 
-    for i in list_of_Forces:
-        new_moment_eq = moment_eq_at_point(list_of_Forces[i].get_x_location,
-                                           list_of_Forces[i].get_y_location,
+    for i in range(len(list_of_Forces)):
+        new_moment_eq = moment_eq_at_point(list_of_Forces[i].get_x_location(),
+                                           list_of_Forces[i].get_y_location(),
                                            list_of_Forces,
                                            list_of_Moments)
-        np.append(a_matrix, new_moment_eq)
+        a_matrix.append(new_moment_eq)
+
+
     return a_matrix
 
-def find_b_matrix(list_of_Forces, list_of_Moments):
-    
 
+def find_b_matrix(list_of_Forces, list_of_Moments):
+    b_matrix = [[sum_forces_x(list_of_Forces)], [sum_forces_y(list_of_Forces)]]
+
+    moment_locations = []
+
+    for i in range(len(list_of_Forces)):
+        new_moment_sum = [sum_moments_at_point(list_of_Forces[i].get_x_location(),
+                                               list_of_Forces[i].get_y_location(),
+                                               list_of_Forces,
+                                               list_of_Moments)]
+        current_location = (list_of_Forces[i].get_x_location(), list_of_Forces[i].get_y_location())
+
+        b_matrix.append(new_moment_sum)
+        moment_locations.append((list_of_Forces[i].get_x_location(), list_of_Forces[i].get_y_location()))
+
+    return b_matrix
+
+
+def solve(a_matrix, b_matrix):
+
+    # creates a new matrix with out any zero equations
+    index = []
+    new_a_matrix = []
+    for i in range(len(a_matrix)):
+        for ii in range(len(a_matrix[i])):
+            # print(a_matrix[i][ii])
+            if abs(a_matrix[i][ii]) > .001:
+                index.append(i)
+                new_a_matrix.append(a_matrix[i])
+                break
+
+    # creates new b matrix to match a matrix
+    new_b_matrix = []
+    for i in index:
+        new_b_matrix.append(b_matrix[i])
+
+    # creates an equation with no repeat equations
+    index_2 = []
+    new_a_matrix_2 = []
+    for i in range(len(new_a_matrix)):
+        if new_a_matrix[i] not in new_a_matrix_2:
+            index_2.append(i)
+            new_a_matrix_2.append(new_a_matrix[i])
+
+    # creates new b matrix to match a matrix again
+    new_b_matrix_2 = []
+    for i in index_2:
+        new_b_matrix_2.append(new_b_matrix[i])
+
+
+    empty_columns = []
+    for i in range(len(new_a_matrix_2)):
+        counter = 0
+        for ii in range(len(new_a_matrix_2[i])):
+            if new_a_matrix_2[ii][i] in new_a_matrix_2[:][i]:
+                counter += 1
+                if counter == len(new_a_matrix_2[i]):
+                    empty_columns.append(i)
+
+    print(empty_columns)
+    for i in sorted(empty_columns, reverse=True):
+        for ii in range(len(new_a_matrix_2)):
+            for iii in range(len(new_a_matrix_2[ii])):
+                print(i)
+                new_a_matrix_2[ii].pop(i)
+                break
+
+    for i in sorted(empty_columns, reverse=True):
+        new_b_matrix_2.pop(i)
+        new_a_matrix_2.pop(i)
+
+
+
+    list_of_answers = np.matmul(inv(new_a_matrix_2), new_b_matrix_2)
+    return list_of_answers
 
 list_of_Forces = []
 list_of_Moments = []
 
-new_force = Force('down', 40, list_of_Forces, angle_from_axis=91, x_location=6)
+new_force = Force('down', 100, list_of_Forces, angle_from_axis=90, x_location=1)
 
-new_force1 = Force('down', 34, list_of_Forces, angle_from_axis=45, x_location=5)
+new_force1 = Force('up', 90, list_of_Forces, angle_from_axis=90, x_location=2)
 
-new_force2 = Force('down', 5, list_of_Forces, angle_from_axis=45, x_location=4)
+new_force4 = Force('down', 40, list_of_Forces, angle_from_axis=90, x_location=.5)
+
+new_force2 = Force('up', 'find', list_of_Forces, angle_from_axis=90, x_location=0)
+
+new_force3 = Force('up', 'find', list_of_Forces, angle_from_axis=90, x_location=2)
 # print(new_force.x_component)
 # print(new_force.y_component)
 # [print(object.x_component) for object in list_of_Forces]
@@ -244,4 +316,48 @@ new_force2 = Force('down', 5, list_of_Forces, angle_from_axis=45, x_location=4)
 #
 # sm = sum_moments_at_point(0, 0, list_of_Moments, list_of_Forces)
 # print(sm)
+# print(sum_moments_at_point(list_of_Forces[1].get_x_location(), list_of_Forces[1].get_y_location, list_of_Forces, list_of_Moments))
+# print(list_of_Forces[2].get_x_location())
+
+
+
+# print(sum_forces_y(list_of_Forces))
+# print(sum_forces_x(list_of_Forces))
+#
+b = find_b_matrix(list_of_Forces, list_of_Moments)
+a = find_a_matrix(list_of_Forces, list_of_Moments)
+
+print(solve(a,b))
+#print(a)
+# print(b)
+
+'''
+b_matrix
+
+x forces
+y forces
+moments about 1
+moments about 2
+moments about 0
+moments about 2
+'''
+
+'''
+a_matrix
+
+[f0] [f1] [f2] [f3] [sum in x or y or moment]
+'''
+
+# print(forces_x(list_of_Forces))
+# print(forces_y(list_of_Forces))
+# print(moment_eq_at_point(list_of_Forces[2].get_x_location(), list_of_Forces[2].get_y_location(), list_of_Forces, list_of_Moments))
+
+#
+# print(moment_eq_at_point(list_of_Forces[1].get_x_location(),
+#                                            list_of_Forces[1].get_y_location(),
+#                                            list_of_Forces,
+#                                            list_of_Moments))
+
+
+
 
